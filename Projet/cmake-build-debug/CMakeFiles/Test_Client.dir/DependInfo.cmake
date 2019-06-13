ee73a3728cd6d2a3397d5e5a38e4454220a97fc6
Valentin POLLART <pollart.valentin@hotmail.fr>
2019-06-13 14:02:18 +0200

    Still working

---
+++ Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/magister/Documents/PSE/Projet/client.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/client.c.o"
+  "/home/magister/Documents/PSE/Projet/modules/datathread.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/modules/datathread.c.o"
+  "/home/magister/Documents/PSE/Projet/modules/erreur.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/modules/erreur.c.o"
+  "/home/magister/Documents/PSE/Projet/modules/ligne.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/modules/ligne.c.o"
+  "/home/magister/Documents/PSE/Projet/modules/msg.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/modules/msg.c.o"
+  "/home/magister/Documents/PSE/Projet/modules/msgbox.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/modules/msgbox.c.o"
+  "/home/magister/Documents/PSE/Projet/modules/resolv.c" "/home/magister/Documents/PSE/Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/modules/resolv.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "../modules"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Projet/cmake-build-debug/CMakeFiles/Test_Client.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_Client.dir/modules/datathread.c.o"
+  "CMakeFiles/Test_Client.dir/modules/erreur.c.o"
+  "CMakeFiles/Test_Client.dir/modules/ligne.c.o"
+  "CMakeFiles/Test_Client.dir/modules/msgbox.c.o"
+  "CMakeFiles/Test_Client.dir/modules/msg.c.o"
+  "CMakeFiles/Test_Client.dir/modules/resolv.c.o"
+  "CMakeFiles/Test_Client.dir/client.c.o"
+  "Test_Client.pdb"
+  "Test_Client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Test_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
